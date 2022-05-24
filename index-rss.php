17f20e24578168f89e856c2fd2ba66f1d5a1d296
James W. Bradley <jwbradley@users.noreply.github.com>
2022-05-24 17:38:25 -0500

    Removed comments
    Comments Out

--- index-rss.php
+++ index-rss.php
@@ -1,28 +1,15 @@
 <?php
 			
-			include('./php/class.bufferapp.php' );
+include('./php/class.bufferapp.php' );
 			
 
-// header('Content-Type: text/xml');
-
-// echo '<META HTTP-EQUIV="refresh" CONTENT="1800">';
-
-/* echo "<?xml version=\"1.0\"?>\n";  */
 echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 echo "<rss version=\"2.0\">\n";
-// <feed xmlns=\"http://www.w3.org/2005/Atom\">\n";
-// echo "<channel>\n\t<atom:link href=\"http://kchoptalk.com/index-rss.php\" rel=\"self\" type=\"application/atom+xml\" />\n";
+
 echo "<channel>\n";
 echo "\t<title>KC Hop Talk</title>\n";
 echo "\t<link>http://kchoptalk.com</link>\n";
 echo "\t<description>KC Hop Talk is Beer News for Beer Nerds. Current Craft Beer News for Kansas City Hop Heads seeking information pertaining to Beer.</description>\n";
-// echo "\n\t<link rel=\"self\" href=\"http://kchoptalk.com/index-rss.php\" />\n";
-
-// echo "\t<subtitle>KC Hop Talk is Beer News for Beer Nerds. Current Craft Beer News for Kansas City Hop Heads seeking information pertaining to Beer.</subtitle>\n";
-// echo "\t<link href=\"http://kchoptalk.com\"/>\n";
-// echo "\t<author>\n\t\t<name>KC Hop Talk</name>\n\t</author>\n";
-// echo "\t<id>http://kchoptalk.com/</id>\n";
-// echo "\t<updated>".date("Y-m-d\Th:i:sP")."</updated>\n";
 
 $debugger     =  ((!isset($_GET['debug']))  ? ''    : htmlspecialchars($_GET['debug']));
 
@@ -33,57 +20,34 @@ $cur_day      =  '';
 $postCounter  =  0;
 $prevlink      =  '';
 
-if (is_array($ret) || is_object($ret))
-{
-	foreach($ret as $key2=>$allLinkData)
-	{
-		if (is_array($allLinkData))
-		{
+if (is_array($ret) || is_object($ret)) {
+	foreach($ret as $key2=>$allLinkData) {
+		if (is_array($allLinkData)) {
 			$rssOutputText = "";
-			foreach($allLinkData as $key3=>$value)
-			{
-				// echo "\n<!-- this \$key3 is $key3 with \$value -- ";
-				// var_dump($value);
-				// echo "  -->\n";
-				// if(strrpos($value, 'https://') >0 ) {
-				// 	echo "\n<!-- if loop nest 1 -->\n ";
-				// 	$link      =  htmlspecialchars( substr($value['text'], strrpos($value['text'], 'https://'), strlen($value['text'])-strrpos($value['text'], 'https://') ) ) ;
-				// 	$PageTitle =  substr($value['text'], 0, strrpos($value['text'], 'https://') );                  
-				// } elseif (strrpos($value, 'http://') >0 ) {
-				// 	echo "\n<!-- if loop nest 2 -->\n ";
-				// 	$link    = htmlspecialchars( substr($value['text'], strrpos($value['text'], 'http://'),  strlen($value['text'])-strrpos($value['text'], 'http://' ) ) ) ;
-    //                 $PageTitle    = substr($value['text'], 0, strrpos($value['text'], 'http://' ) );
-          
-				
-				if ((strrpos($value['text'], 'https://') != 0 ) || (strrpos($value['text'], 'http://') != 0 ) ) { 
-					  // echo "\n<!-- if loop nest 2 -->\n ";
-	                  $link    = htmlspecialchars( $value['media']["link"] ) ;
-	                  $PageTitle   = $value['media']['title'];
-	                  $hrefDesc    = $value['media']['description'];
-	                  $hrefThumb   = $value['media']['thumbnail'];
-                } else if ((strrpos($value['text'], 'https://') == 0 ) && (strrpos($value['text'], 'http://') == 0 ) ) { 
-					  // echo "\n<!-- if loop nest 3 -->\n ";
-	                  $link    = htmlspecialchars( $value['media']["link"] ) ;
-	                  $PageTitle   = $value['text'];
-	                  $hrefTitle   = ($value['media']['title'] == '404' ? '' : $value['media']['title']);
-	                  $hrefDesc    = ($value['media']['title'] == '404' ? '' : $value['media']['description']);
-	                  $hrefThumb   = $value['media']['thumbnail'];
-                } 
-				// if ( ($link) && ($link !== $prevlink) ) {
-				// if ( ($link) && ($link !== $prevlink) && (!array_search($link, array_column($people, 'fav_color')) ) ) {
-				if ( ($link) && ($PageTitle) && ($hrefDesc) && ($PageTitle != 'null') && ($hrefDesc != 'null') && ($link !== $prevlink) && (!strpos($rssOutputText, $link) ) ) {
-					$rssOutputText .= "\t<item>\n";
-					$rssOutputText .= "\t\t<title>".trim(htmlspecialchars($PageTitle))."</title>\n";
-					$rssOutputText .= "\t\t<description>".htmlspecialchars(trim($hrefDesc))."</description>\n";
-					$rssOutputText .= "\t\t<link>".trim(htmlspecialchars($link)). "</link>\n";
-					$rssOutputText .= "\t</item>\n";
-					// echo "\t<item>\n";
-					// echo "\t\t<title>".trim(htmlspecialchars($PageTitle))."</title>\n";
-					// echo "\t\t<description>".htmlspecialchars(trim($hrefDesc))."</description>\n";
-					// echo "\t\t<link>".trim(htmlspecialchars($link)). "</link>\n";
-					// echo "\t</item>\n";
-					$prevlink = $link;
-				}									
+			foreach($allLinkData as $key3=>$value) {
+							
+			  if ((strrpos($value['text'], 'https://') != 0 ) || (strrpos($value['text'], 'http://') != 0 ) ) { 
+			
+	                  	$link    = htmlspecialchars( $value['media']["link"] ) ;
+	                  	$PageTitle   = $value['media']['title'];
+	                  	$hrefDesc    = $value['media']['description'];
+	                  	$hrefThumb   = $value['media']['thumbnail'];
+                          } else if ((strrpos($value['text'], 'https://') == 0 ) && (strrpos($value['text'], 'http://') == 0 ) ) { 
+			  	$link    = htmlspecialchars( $value['media']["link"] ) ;
+	                  	$PageTitle   = $value['text'];
+	                  	$hrefTitle   = ($value['media']['title'] == '404' ? '' : $value['media']['title']);
+	                  	$hrefDesc    = ($value['media']['title'] == '404' ? '' : $value['media']['description']);
+	                  	$hrefThumb   = $value['media']['thumbnail'];
+                	  } 
+			
+			  if ( ($link) && ($PageTitle) && ($hrefDesc) && ($PageTitle != 'null') && ($hrefDesc != 'null') && ($link !== $prevlink) && (!strpos($rssOutputText, $link) ) ) {
+				$rssOutputText .= "\t<item>\n";
+				$rssOutputText .= "\t\t<title>".trim(htmlspecialchars($PageTitle))."</title>\n";
+				$rssOutputText .= "\t\t<description>".htmlspecialchars(trim($hrefDesc))."</description>\n";
+				$rssOutputText .= "\t\t<link>".trim(htmlspecialchars($link)). "</link>\n";
+				$rssOutputText .= "\t</item>\n";
+				$prevlink = $link;
+			  }									
 			}
 			echo $rssOutputText;
 		}
@@ -92,13 +56,5 @@ if (is_array($ret) || is_object($ret))
 
 echo "\n</channel>\n";
 echo "</rss>\n";
-
-
-
-// if($debugger) {
-// 	echo "\n<!-- this is the return from buffer -- ";
-// 	var_dump($ret);
-// 	echo "  -->\n";
-// }
 
 ?>
